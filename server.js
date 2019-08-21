9c34ec6831918dd6c590fbdf5d3f7db57262d47d
AgnLiska <agiusia@gmail.com>
2019-08-21 10:02:09 +0200

    Remove the now ignored directory node_modules

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.json('it is working :)');
+	res.json('it is working');
 })
 
 app.post('/signin', signin.handleSignIn(db, bcrypt))
